2d81f3bf9c565d271e9d029d5959ca4b4f35e36e
Gopinath P <gopinath.p@tandemloop.in>
2024-08-29 11:12:00 +0530

    INIT: Dependencies added and theme configured

--- src/components/layout/layout.component.ts
+++ src/components/layout/layout.component.ts
@@ -1,12 +1,17 @@
+//Core
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLinkActive } from '@angular/router';
 
+//PrimeNG
+import { ButtonModule } from 'primeng/button';
+
+const CORE = [RouterOutlet, RouterLinkActive];
+const PRIMENG = [ButtonModule];
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [...CORE, ...PRIMENG],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
